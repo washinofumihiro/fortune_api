d841822b24f2ebfefe2a7b513e3c75b2af7f1aaf
smap11 <smap11@smap11noMacBook-Pro.local>
2017-05-12 15:43:03 +0900

    テスト用のモデルを作成

--- fortune_telling/fortune_result/models.py
+++ fortune_telling/fortune_result/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class result(models.Model):
+
+    name = models.CharField('占い結果')
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
