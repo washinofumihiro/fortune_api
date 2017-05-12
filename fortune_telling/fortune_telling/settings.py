731651f39167126541ab196427c94259cf904df6
smap11 <smap11@smap11noMacBook-Pro.local>
2017-05-12 15:53:59 +0900

    モデルの有効化と言語とタイムゾーンの設定

--- fortune_telling/fortune_telling/settings.py
+++ fortune_telling/fortune_telling/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fortune_result',
 ]
 
 MIDDLEWARE = [
@@ -124,9 +125,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
