d9e2bab98834f5dcfedd560015c4484a9a60f8d8
smap11 <smap11@smap11noMacBook-Pro.local>
2017-05-15 10:11:03 +0900

    モデルとビューを作成

--- fortune_telling/fortune_result/models.py
+++ fortune_telling/fortune_result/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Result(models.Model):
 
     # name = models.CharField('占い結果')
-    name = models.TextField('占い結果')
+    result = models.TextField('占い結果')
 
     def __str__(self):
-        return self.name
+        return self.result
 
 
 # Create your models here.
--- fortune_telling/fortune_result/views.py
+++ fortune_telling/fortune_result/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def get_fortune(request):
+    # getメソッドの作成
+    return HttpResponse('get')
