bd2807cd63c0618e7cf01f8c6cbc378d3d7c67ef
smap11 <smap11@smap11noMacBook-Pro.local>
2017-05-15 09:14:09 +0900

    Bootstrapを使用するために記述

--- fortune_telling/fortune_telling/settings.py
+++ fortune_telling/fortune_telling/settings.py
@@ -142,3 +142,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 静的ファイルを共通で置く
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
