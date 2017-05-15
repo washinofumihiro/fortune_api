c770dc31fd537e3de6b366a08df0d481dd9857fd
smap11 <smap11@smap11noMacBook-Pro.local>
2017-05-15 09:12:17 +0900

    DBのテストのためmygrateを行い、管理サイトから直接触れるように更新

--- fortune_telling/fortune_result/models.py
+++ fortune_telling/fortune_result/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 
-class result(models.Model):
+class Result(models.Model):
 
-    name = models.CharField('占い結果')
+    # name = models.CharField('占い結果')
+    name = models.TextField('占い結果')
 
     def __str__(self):
         return self.name
