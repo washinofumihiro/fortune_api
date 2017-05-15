4fd84b9fab404c983e7acdc8c720773f9e55a723
smap11 <smap11@smap11noMacBook-Pro.local>
2017-05-15 15:43:41 +0900

    modelの変更に伴い、serializerを変更

--- fortune_telling/fortune_result/serializer.py
+++ fortune_telling/fortune_result/serializer.py
@@ -2,10 +2,19 @@
 
 from rest_framework import serializers
 
-from .models import Result
+from .models import Result, BloodType
+
+
+class BloodTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BloodType
+        fields = ('b_type',)
 
 
 class ResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = Result
-        fields = ('result',)
+        fields = ('result', 'date')
+
+
+
