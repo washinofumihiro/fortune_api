6343ca5a656fdd339b2b98932df97a8876fb22fb
smap11 <smap11@smap11noMacBook-Pro.local>
2017-05-16 09:11:18 +0900

    ターミナル上にDB内容を表示できるかのテスト

--- fortune_telling/fortune_result/views.py
+++ fortune_telling/fortune_result/views.py
@@ -9,10 +9,20 @@ from .serializer import BloodTypeSerializer, ResultSerializer
 
 class BloodTypeViewSet(viewsets.ModelViewSet):
     queryset = BloodType.objects.all()
+    # queryset = BloodType.objects.all().prefetch_related("entries").get(id=1).entries.all()
     serializer_class = BloodTypeSerializer
+    # for blood_type in BloodType.objects.all().prefetch_related("entries"):
+    #     print(blood_type.get(id=1).entries.all())
+    # print(BloodType.objects.all().prefetch_related("entries").get(id=2))
+    # print(Result.objects.all().result)
+    report = Result.objects.all()
+    print(report.get(date=20170520).date)
+
+    print("test")
 
 
 class ResultViewSet(viewsets.ModelViewSet):
     queryset = Result.objects.all()
     serializer_class = ResultSerializer
+    # print(Result.objects.all()[0])
 
